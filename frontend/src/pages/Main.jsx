96797747d1c2a2a11fb3f1697242e77ba87285fd
kmgnw <wathcpropropropro@gmail.com>
2024-07-22 17:14:40 +0900

    🐛 플로팅 버튼 위치 수정

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -54,8 +54,8 @@ position: relative
 
 const FloatingChatbotWrap = styled.div`
 position: absolute;
-bottom: 1rem;
-right:1rem;
+bottom: 11.7rem;
+right:2rem;
 display: flex;
 width: 6rem;
 height: 6rem;
@@ -68,6 +68,12 @@ border-radius: 3rem;
 background: #000;
 box-shadow: 0px 8px 8px 0px rgba(0, 0, 0, 0.20);
 ${hoverGrow}
+
+@media (max-width: 428px){
+position: fixed;
+bottom: 3.7rem;
+right:2rem;
+}
 `
 
 export default Main;
