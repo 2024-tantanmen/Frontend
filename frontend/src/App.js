944b787dc4b3d92f848ccde59560064341d4a3f6
kmgnw <wathcpropropropro@gmail.com>
2024-07-22 21:30:46 +0900

    ✨ 식단 추천 페이지 생성 & 라우팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import AddSchedule from "./pages/AddSchedule";
 import CarbCounting from "./pages/CarbCounting";
+import DietRecommend from "./pages/DietRecommend";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Main />}/>
       <Route path="add-schedule" element={<AddSchedule />} />
       <Route path="carb-counting" element={<CarbCounting/>} />
+      <Route path="diet-recommend" element={<DietRecommend/>} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/DietRecommend.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import Header from "../entities/DietRecommend/ui/Header";
+
+function DietRecommend(){
+    return(
+        <Header />
+    )
+}
+
+export default DietRecommend;
