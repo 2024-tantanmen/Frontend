2598c9ebc03cb564ab4679d7bad857d4ca670065
kmgnw <127284120+kmgnw@users.noreply.github.com>
2024-07-23 15:38:42 +0900

    Feat/#27 diet recommend (#31)
    * ✨ 식단 추천 페이지 생성 & 라우팅
    
    * ✨ 식단 추천 결과 페이지 레이아웃 구현
    
    * ✨ 식단 추천 페이지 레이아웃 구현
    
    * ✨ 식단 추천 페이지 구현
    
    ---------
    
    Co-authored-by: kmgnw <wathcpropropropro@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import AddSchedule from "./pages/AddSchedule";
 import CarbCounting from "./pages/CarbCounting";
+import DietRecommend from "./pages/DietRecommend";
+import DietRecommendResult from "./pages/DietRecommendResult";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Main />}/>
       <Route path="add-schedule" element={<AddSchedule />} />
       <Route path="carb-counting" element={<CarbCounting/>} />
+      <Route path="diet-recommend" element={<DietRecommend/>} />
+      <Route path="diet-recommend-result" element={<DietRecommendResult/>} />
     </Routes>
   );
 }
