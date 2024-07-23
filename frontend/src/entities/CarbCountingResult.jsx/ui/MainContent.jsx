e2369272e03eb8479e829c368838cf02b8d9f3bc
sengooooo <lesley023@naver.com>
2024-07-23 11:57:22 +0900

    ✨ FEAT. Carousel 제작

--- frontend/src/entities/CarbCountingResult.jsx/ui/MainContent.jsx
+++ frontend/src/entities/CarbCountingResult.jsx/ui/MainContent.jsx
@@ -1,68 +1,79 @@
 import styled from "styled-components";
-import { useRecoilValue } from 'recoil';
-import { selectedImgState } from '../../../shared/state/Gallery';
-import test from '../../../assets/testImg.svg';
-import icon from '../../../assets/pretzelIcon.svg';
-import linkIcon from '../../../assets/linkIcon.svg';
-import kakaoIcon from '../../../assets/kakaoIcon.svg';
-import StandardButton from '../../../shared/components/StandardButton/StandardButton';
+import { useRecoilValue } from "recoil";
+import { selectedImgState } from "../../../shared/state/Gallery";
+import test from "../../../assets/testImg.svg";
+import icon from "../../../assets/pretzelIcon.svg";
+import linkIcon from "../../../assets/linkIcon.svg";
+import kakaoIcon from "../../../assets/kakaoIcon.svg";
+import StandardButton from "../../../shared/components/StandardButton/StandardButton";
+import Carousel from "./Carousel";
 import { useState } from "react";
 import { hoverGrow } from "../../../shared/animation/hoverGrow";
+import MenuCarousel from "./MenuCarousel";
 
+function MainContent() {
+  const selectedImg = useRecoilValue(selectedImgState);
+  const [foodCnt, setFoodCnt] = useState("00"); //음식개수
+  const [calCnt, setcalCnt] = useState("000"); //칼로리
 
-function MainContent(){
-
-    const selectedImg = useRecoilValue(selectedImgState);
-    const [foodCnt, setFoodCnt] = useState('00');
-    const [calCnt, setcalCnt] = useState('000');
-
-
-    return(
+  return (
     <Wrapper>
-        <Title>업로드한 사진</Title>
-        <PicContainer>
-            <Img src={selectedImg}/>
-        </PicContainer>
+      <Title>업로드한 사진</Title>
+      <PicContainer>
+        <Img src={selectedImg} />
+      </PicContainer>
 
-        <ResultContainer>
-        <TitleIcon src={icon}/>
+      <ResultContainer>
+        <TitleIcon src={icon} />
         <ResultTitle>식단 분석 결과</ResultTitle>
-        </ResultContainer>
+      </ResultContainer>
 
-        <CntContainer>
+      <CntContainer>
         <FontContainer>총</FontContainer>
         <FoodCntContainer>{foodCnt}</FoodCntContainer>
         <FontContainer>개의 음식</FontContainer>
-        </CntContainer>
+      </CntContainer>
 
-        <CntContainer style={{marginTop:"0.8rem"}}>
+      <CntContainer style={{ marginTop: "0.8rem" }}>
         <FontContainer>총</FontContainer>
         <FoodCntContainer>{calCnt}</FoodCntContainer>
         <FontContainer>kcal</FontContainer>
-        </CntContainer>
+      </CntContainer>
 
-        <FontContainer style={{marginTop:"4rem", marginLeft:"2rem"}}>식단 총 함량 분석</FontContainer>
-        {/** 여기에 캐러셀 추가 */}
+      <FontContainer style={{ marginTop: "4rem", marginLeft: "2rem" }}>
+        식단 총 함량 분석
+      </FontContainer>
+      <Carousel />
 
-        <FontContainer style={{marginTop:"4.4rem", marginLeft:"2rem"}}>메뉴별 분석&nbsp;<span style={{color:"#EF6038"}}>{foodCnt}</span></FontContainer>
-        {/** 여기에 캐러셀 추가 */}
+      <FontContainer style={{ marginTop: "3.2rem", marginLeft: "2rem" }}>
+        메뉴별 분석&nbsp;<span style={{ color: "#EF6038" }}>{foodCnt}</span>
+      </FontContainer>
+      <MenuCarousel />
 
-        <ShareContainer>
-          <FontContainer style={{color:"#fff", position:"absolute", left:"2rem"}}>분석 결과를 공유해보세요</FontContainer>
+      <ShareContainer>
+        <FontContainer
+          style={{ color: "#fff", position: "absolute", left: "2rem" }}
+        >
+          분석 결과를 공유해보세요
+        </FontContainer>
 
-          <IconContainer>
-            <Icon src={linkIcon}/>
-            <Icon src={kakaoIcon}/>
-          </IconContainer>
-        </ShareContainer>
+        <IconContainer>
+          <Icon src={linkIcon} />
+          <Icon src={kakaoIcon} />
+        </IconContainer>
+      </ShareContainer>
 
-        <ButtonContainer>
-        <StandardButton title="다시 해보기" width="18.7rem" height="4rem"/>
-        <StandardButton title="총 칼로리 기록하기" width="18.7rem" height="4rem" backgroundColor="#EF6038"/>
+      <ButtonContainer>
+        <StandardButton title="다시 해보기" width="18.7rem" height="4rem" />
+        <StandardButton
+          title="총 칼로리 기록하기"
+          width="18.7rem"
+          height="4rem"
+          backgroundColor="#EF6038"
+        />
       </ButtonContainer>
-        
-      </Wrapper>
-      );
+    </Wrapper>
+  );
 }
 
 export default MainContent;
@@ -83,11 +94,11 @@ const Title = styled.div`
 `;
 
 const Img = styled.img`
-    border-radius: 16px;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-`
+  border-radius: 16px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 const PicContainer = styled.div`
   margin: 0.8rem 2rem 0 2rem;
@@ -99,77 +110,76 @@ const PicContainer = styled.div`
 `;
 
 const TitleIcon = styled.img`
-    width: 3.2rem;
-    height: 3.2rem;
-    margin-right: 8px;
-`
+  width: 3.2rem;
+  height: 3.2rem;
+  margin-right: 8px;
+`;
 
 const ResultTitle = styled.div`
-color: var(--Gray8, #262829);
-font-family: "Noto Sans KR";
-font-size: 20px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-
-`
+  color: var(--Gray8, #262829);
+  font-family: "Noto Sans KR";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
 
 const ResultContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-left: 2rem;
-    margin-top: 4.6rem;
-`
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+  margin-top: 4.6rem;
+`;
 
 const FontContainer = styled(ResultTitle)`
-font-size: 16px;
-`
+  font-size: 16px;
+`;
 
 const FoodCntContainer = styled(FontContainer)`
-display: flex;
-width: fit-content;
-padding: 8px 16px;
-justify-content: center;
-align-items: center;
-border-radius: 8px;
-border: 1px solid var(--Gray2, #E3E5E7);
-background: var(--White, #FFF);
-color: var(--Primary-color1, #EF6038);
-`
+  display: flex;
+  width: fit-content;
+  padding: 8px 16px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  border: 1px solid var(--Gray2, #e3e5e7);
+  background: var(--White, #fff);
+  color: var(--Primary-color1, #ef6038);
+`;
 
 const CntContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-left: 2rem;
-    margin-top: 2.4rem;
-    gap: 0.8rem;
-`
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+  margin-top: 2.4rem;
+  gap: 0.8rem;
+`;
 
 const ShareContainer = styled.div`
   position: relative;
-  display:flex;
+  display: flex;
   width: 100%;
   height: 8rem;
   background: var(--Black, #000);
-  margin-top: 4rem;
+  margin-top: 2rem;
   align-items: center;
-`
+`;
 
 const IconContainer = styled.div`
-display:flex;
-position: absolute;
-justify-content: center;
-align-items:center;
-gap: 0.8rem;
-right: 1.8rem;
-`
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+  right: 1.8rem;
+`;
 
 const Icon = styled.img`
-width: 4.8rem;
-height: 4.8rem;
-cursor: pointer;
-${hoverGrow}
-`
+  width: 4.8rem;
+  height: 4.8rem;
+  cursor: pointer;
+  ${hoverGrow}
+`;
 
 const ButtonContainer = styled.div`
   background: #fff;
