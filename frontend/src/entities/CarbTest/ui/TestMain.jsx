e4c0c7b943abfa9e29e3c90ab18374c0621fc0e0
kmgnw <wathcpropropropro@gmail.com>
2024-07-25 10:10:37 +0900

    🚑️ 마이페이지" 달력으로 이동" 라우팅

--- frontend/src/entities/CarbTest/ui/TestMain.jsx
+++ frontend/src/entities/CarbTest/ui/TestMain.jsx
@@ -6,16 +6,19 @@ export default function TestMain(){
 
     return(
     <Wrapper>
+
         <TitleContainer>
             <Icon src={icon}/>
             <div>자신에게 해당하는 질문에 체크하세요.</div>
         </TitleContainer>
+
         <TestContent/>
+
     </Wrapper>);
 }
 
 const Wrapper = styled.div`
-background: var(--Gray1, #F2F3F5);
+background: #F2F3F5;
 padding: 2.5rem 2rem 1.6rem 2rem;
 `
 
@@ -32,6 +35,6 @@ line-height: normal;
 `
 
 const Icon=styled.img`
-    width: 3.2rem;
-    height: 3.2rem;
+width: 3.2rem;
+height: 3.2rem;
 `
